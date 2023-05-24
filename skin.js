0181c2a1bde2e5a9966101a2923f22778ae632b5
Kami1aa <126070267+Kami1aa@users.noreply.github.com>
2023-05-24 21:57:05 +0600

    Create skin.js

---
+++ skin.js
@@ -0,0 +1,9 @@
+var product = {
+  name: "Cleansing Gel",
+  brand: "Cerave",
+  description: "A gentle cleanser for everyday use.",
+  price: 15.99
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+});
